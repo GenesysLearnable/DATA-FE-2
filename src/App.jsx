67bcb945d19ba87a41ff7a8c73ce0123bb540c10
Patochukwu <christiantus22@gmail.com>
2024-05-16 02:18:46 +0100

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import './App.css'
-import Signin from './pages/Login'
-import { Signup } from "./pages/Signup"
-import { EmailVerification } from "./pages/EmailVerification"
-import { Homepage } from "./pages/Homepage"
-import { Onboarding } from "./pages/Onboarding"
-import { ForgotPassword } from "./pages/ForgotPassword"
 
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Signup />}/>
-          <Route path="SignUp" element={<Signup/>}/>
-          <Route path="Login" element={<Signin/>}/>
-          <Route path="EmailVerification" element={<EmailVerification/>}/>
-          <Route path="Homepage" element={<Homepage/>}/>
-          <Route path="Onboarding" element={<Onboarding/>}/>
-          <Route path="ForgotPassword" element={<ForgotPassword/>}/>
-          {/* <Route path="*" element={<PageNotFound />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
 
-export default App
