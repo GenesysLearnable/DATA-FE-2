e4577088d1a53e3440a9749b9dac6f2995d5ccfe
CrownedTechie <nenchi00@gmail.com>
2024-05-20 15:11:46 +0100

    create separate file for the media sources

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,40 +1,58 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
+import hulu from '/public/Media 1.png';
+import { MEDIA_SOURCES } from '../components/data/data';
 
 export const Onboarding = () => {
+  const [openPopUp, setOpenPopUp] = useState(false);
+  const [selectedAccount, setSelectedAccount] = useState(null);
+
+  function handleDoneClick() {
+    setOpenPopUp(true);
+  }
+
   return (
-    <main className='mainStyle'>
-      <header className='mediaLogoDiv'>
-        <img src="/Media Hub Logo 2 1.png" alt="logo" />
-      </header>
-      <section className='onboarding'>
-        <div className='mediah1pDiv'>
-          <h1>Select your Media accounts</h1>
-          <p>Kindly select the accounts you want to connect to Media hub</p>
-        </div>
-        <section className='mediaSection'>
-          <div>
-            <Link><img src="/public/Media 1.png" alt="" /></Link>
-            <Link><img src="/public/Media 2.png" alt="" /></Link>
-            <Link><img src="/public/Media 3.png" alt="" /></Link>
+    <>
+      <main className='mainStyle'>
+
+        <header className='mediaLogoDiv'>
+          <img src="/Media Hub Logo 2 1.png" alt="logo" />
+        </header>
+
+        <section className='onboarding'>
+          <div className='mediah1pDiv'>
+            <h1>Select your Media accounts</h1>
+            <p>Kindly select the accounts you want to connect to Media hub</p>
           </div>
-          <div>
-            <Link><img src="/public/Media 4.png" alt="" /></Link>
-            <Link><img src="/public/Media 5.png" alt="" /></Link>
-            <Link><img src="/public/Media 6.png" alt="" /></Link>
-          </div>
-          <div>
-            <Link><img src="/public/Media 7.png" alt="" /></Link>
-            <Link><img src="/public/Media 8.png" alt="" /></Link>
-            <Link><img src="/public/Media 9.png" alt="" /></Link>
+
+          <section className='mediaSection'>
+            <div>
+              <Link><img src={hulu} alt="" /></Link>
+              <Link><img src="/public/Media 2.png" alt="" /></Link>
+              <Link><img src="/public/Media 3.png" alt="" /></Link>
+            </div>
+
+            <div>
+              <Link><img src="/public/Media 4.png" alt="" /></Link>
+              <Link><img src="/public/Media 5.png" alt="" /></Link>
+              <Link><img src="/public/Media 6.png" alt="" /></Link>
+            </div>
+
+            <div>
+              <Link><img src="/public/Media 7.png" alt="" /></Link>
+              <Link><img src="/public/Media 8.png" alt="" /></Link>
+              <Link><img src="/public/Media 9.png" alt="" /></Link>
+            </div>
+          </section>
+
+          <div className='mediaButtonDiv'>
+            <button>Skip</button>
+            <button className='mediaDoneButton' onClick={handleDoneClick}>Done</button>
           </div>
         </section>
-        <div className='mediaButtonDiv'>
-          <button>Skip</button>
-          <button className='mediaDoneButton'>Done</button>
-        </div>
-      </section>
-    </main>
-         
+
+      </main>
+    
+    </>
   )
 }
