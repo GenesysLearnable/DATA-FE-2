91458e2de99f15f63e62ab14a4dc175cc3783092
CrownedTechie <nenchi00@gmail.com>
2024-05-21 19:15:05 +0100

    src error correction in the movie images import

--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -10,17 +10,17 @@ import youtubeMusic from '/public/Media 9.png';
 
 // MOVIE IMAGES
 import meaCulpa from '/public/mea-culpa.png';
-import kingTheLand from 'public/king-the-land.png';
-import anikulapo from 'anikulapo.png';
-import empire from 'public/empire.png';
-import inheritors from 'public/inheritors.png';
-import purpleHearts from 'public/purple-hearts.png';
-import bossLevel from 'public/bosslevel.png';
-import rrr from 'public/RRR.png';
-import courage from 'public/courage.png';
-import businessProposal from 'public/business-proposal.png';
-import bridgerton from 'public/bridgerton.png';
-import bloodSisters from 'public/blood-sisters.png';
+import kingTheLand from '/public/king-the-land.png';
+import anikulapo from '/anikulapo.png';
+import empire from '/public/empire.png';
+import inheritors from '/public/inheritors.png';
+import purpleHearts from '/public/purple-hearts.png';
+import bossLevel from '/public/boss-level.png';
+import rrr from '/public/RRR.png';
+import courage from '/public/courage.png';
+import businessProposal from '/public/business-proposal.png';
+import bridgerton from '/public/bridgerton.png';
+import bloodSisters from '/public/blood-sisters.png';
 
 
 
