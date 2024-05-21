776c4a9c586a5b7a36a2f7c75ed28993e46c35e3
CrownedTechie <nenchi00@gmail.com>
2024-05-21 19:07:40 +0100

    add movies info to the data file successfully

--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -8,6 +8,22 @@ import soundCloud from '/public/Media 7.png';
 import showTime from '/public/Media 8.png';
 import youtubeMusic from '/public/Media 9.png';
 
+// MOVIE IMAGES
+import meaCulpa from '/public/mea-culpa.png';
+import kingTheLand from 'public/king-the-land.png';
+import anikulapo from 'anikulapo.png';
+import empire from 'public/empire.png';
+import inheritors from 'public/inheritors.png';
+import purpleHearts from 'public/purple-hearts.png';
+import bossLevel from 'public/bosslevel.png';
+import rrr from 'public/RRR.png';
+import courage from 'public/courage.png';
+import businessProposal from 'public/business-proposal.png';
+import bridgerton from 'public/bridgerton.png';
+import bloodSisters from 'public/blood-sisters.png';
+
+
+
 
 export const MEDIA_SOURCES = 
    [ 
@@ -21,3 +37,25 @@ export const MEDIA_SOURCES =
     { name: 'ShowTime', image: showTime },
     { name: 'Youtube Music', image: youtubeMusic },
 ];
+
+export const WEEKLY_MOVIES =
+    [
+        { title: 'Mea Culpa', image: meaCulpa },
+        { title: 'King the Land', image: kingTheLand },
+        { title: 'Anikulapo', image: anikulapo },
+        { title: 'Empire', image: empire },
+    ];
+
+export const MONTHLY_MOVIES = 
+    [
+        { title: 'Inheritors', image: inheritors },
+        { title: 'Purple Hearts', image: purpleHearts },
+        { title: 'Boss Level', image: bossLevel },
+        { title: 'RRR', image: rrr },
+        { title: 'Courage', image: courage },
+        { title: 'Business Proposal', image: businessProposal },
+        { title: 'Bridgerton', image: bridgerton },
+        { title: 'Blood Sisters', image: bloodSisters },
+    ]
+
+    
