713e4d403024669362ad8b2c05cc01d54f98366f
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2024-05-22 14:52:00 +0100

    added profile page, properly indented some parts of signin and removed border radius from toggle button to make them overlap

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Homepage } from "./pages/Homepage"
 import { Onboarding } from "./pages/Onboarding"
 import { ForgotPassword } from "./pages/ForgotPassword"
 // import { LandingPage } from "./pages/LandingPage"
-// import { ProfilePage } from "./pages/ProfilePage"
+import { ProfilePage } from "./pages/ProfilePage"
 
 
 function App() {
@@ -22,8 +22,8 @@ function App() {
           <Route path="Homepage" element={<Homepage/>}/>
           <Route path="Onboarding" element={<Onboarding/>}/>
           <Route path="ForgotPassword" element={<ForgotPassword/>}/>
-          {/* <Route path="LandingPage" element={<LandingPage/>}/>
-          <Route path="ProfilePage" element={<ProfilePage/>}/> */}
+          {/* <Route path="LandingPage" element={<LandingPage/>}/> */}
+          <Route path="ProfilePage" element={<ProfilePage/>}/>
           {/* <Route path="*" element={<PageNotFound />} /> */}
         </Routes>
       </BrowserRouter>
