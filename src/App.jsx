7130283a13a85c45e201296179e7975a54a90715
Patochukwu <christiantus22@gmail.com>
2024-05-23 23:04:33 +0100

    added landingpage and few edits

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { EmailVerification } from "./pages/EmailVerification"
 import { Homepage } from "./pages/Homepage"
 import { Onboarding } from "./pages/Onboarding"
 import { ForgotPassword } from "./pages/ForgotPassword"
-// import { LandingPage } from "./pages/LandingPage"
+import { LandingPage } from "./pages/LandingPage"
 import { ProfilePage } from "./pages/ProfilePage"
 
 
@@ -15,15 +15,15 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Signup />}/>
-          <Route path="SignUp" element={<Signup/>}/>
-          <Route path="Signin" element={<Signin/>}/>
-          <Route path="EmailVerification" element={<EmailVerification/>}/>
-          <Route path="Homepage" element={<Homepage/>}/>
-          <Route path="Onboarding" element={<Onboarding/>}/>
-          <Route path="ForgotPassword" element={<ForgotPassword/>}/>
-          {/* <Route path="LandingPage" element={<LandingPage/>}/> */}
-          <Route path="ProfilePage" element={<ProfilePage/>}/>
+          <Route index element={<LandingPage />} />
+          <Route path="SignUp" element={<Signup />} />
+          <Route path="Signin" element={<Signin />} />
+          <Route path="EmailVerification" element={<EmailVerification />} />
+          <Route path="Homepage" element={<Homepage />} />
+          <Route path="Onboarding" element={<Onboarding />} />
+          {/* <Route path="ForgotPassword" element={<ForgotPassword />} /> */}
+          <Route path="LandingPage" element={<LandingPage />} />
+          <Route path="ProfilePage" element={<ProfilePage />} />
           {/* <Route path="*" element={<PageNotFound />} /> */}
         </Routes>
       </BrowserRouter>
